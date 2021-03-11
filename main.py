052ef5e202f9c1033727cca3762ea1d389965528
Leon Niederprüm <31163998+aveniir@users.noreply.github.com>
2021-03-11 22:12:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import discord
+import tmdbv3api
+from TOKEN import TOKEN, TMDB_TOKEN
+from discord.ext import commands
+from discord.ext.commands.core import command, cooldown
+from tmdbv3api import TMDb
+from tmdbv3api import Movie, TV
+
+#TMDB Setup
+tmdb = TMDb()
+tmdb.api_key = TMDB_TOKEN
+tmdb.language = 'de'
+tmdb.debug = False
+movie = Movie()
+tv = TV()
+
+#Link setup
+link_prefix = 'https://www.themoviedb.org/movie/'
+link_suffix = '?language=de-DE'
+
+#Command Prefix
+bot = commands.Bot(command_prefix='!')
+
+#Main setup
+@bot.event
+async def on_ready():
+    print('Logged in and ready to go.')
+
+#Purge Text Channel command
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def clean(ctx, limit: int):
+        await ctx.channel.purge(limit=limit)
+        await ctx.message.delete()
+
+#Movie-Request
+@bot.command()
+async def mdb(ctx, *args):
+    movieName = args
+    movieName = ' '.join(map(str, movieName))
+    print(movieName)
+    if movieName.startswith('id=') == True:
+        await ctx.channel.send(link_prefix + movieName.removeprefix('id=') + link_suffix)
+    elif len(movieName) == 0:
+        await ctx.channel.send('Nach welchen Film suchst du?\nBsp: !mdb Interstellar')
+    else:
+        search = movie.search(movieName)
+        if len(search) == 0:
+            await ctx.channel.send('Ich konnte den Film', movieName, ' nicht finden.')
+        else:
+            for res in search:
+                await ctx.channel.send(link_prefix + str(res.id) + link_suffix)
+                return
+
+
+#---------- error handling ----------#
+
+#Insufficient permissions to use !clean command
+@clean.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Du bist kein Admin... oder?")
+
+bot.run(TOKEN)
